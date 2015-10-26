657aa495d76cb4d96fb612bb032c55edfc0da3e1
BRUNA\BrunaT <zilda.taty5@gmail.com>
2015-10-26 13:58:02 -0530

    Commit inicial.

---
+++ Assets/Scripts/Play.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Play : MonoBehaviour
+{
+
+    public void PlayButton()
+    {
+        Application.LoadLevel("Mapa");
+    }
+}
---
+++ Assets/Scripts/teste2.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class teste2 : MonoBehaviour
+{
+    public float minSwipeDistY;
+    public float minSwipeDistX;
+    private Vector2 startPos;
+    public float power = 100; //the engine power applied to the wheel
+    public float velocity = 0.00000001f;
+    [SerializeField]
+    GameObject car; //the object whose velocity you are calculating
+
+    void Update()
+    {
+
+        //#if UNITY_ANDROID
+        if (Input.touchCount > 0)
+        {
+
+            Touch touch = Input.touches[0];
+
+            switch (touch.phase)
+            {
+
+                case TouchPhase.Began:
+
+                    startPos = touch.position;
+                    break;
+
+               case TouchPhase.Moved:
+
+                        float swipeDistHorizontal = (new Vector3(touch.position.x, 0, 0) - new Vector3(startPos.x, 0, 0)).magnitude;
+                       // var wheelpower = velocity * power * Time.deltaTime; //velocity of "car" * engine power
+
+                    if (swipeDistHorizontal > minSwipeDistX)
+                        {
+
+                            float swipeValue = Mathf.Sign(touch.position.x - startPos.x);
+
+                            if (swipeValue > 0)
+                            {
+                                transform.Rotate(0, 0, swipeDistHorizontal);
+                            }
+                            else if (swipeValue < 0)
+                            {
+                               transform.Rotate(0, 0, -swipeDistHorizontal);
+                            }
+                        }
+                  break;
+               }
+                    
+           }
+       } 
+}
