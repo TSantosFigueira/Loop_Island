e8fd48b69236c9e88252c6618662c2af552344d4
TSantosFigueira <tsantosfigueira@outlook.com>
2015-10-26 18:35:01 -0400

    Drag & Drop criado.

---
+++ Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnEnemy : MonoBehaviour {
+    // Public gameobject where you will drag the prefab you want to spawn.
+    public GameObject spawnEnemy;
+    public float delayTime = 5f;
+
+
+    void Start()
+    {
+        StartCoroutine(EnemySpawnTimer());
+    }
+
+
+    // Function type that allows us to use wait function.
+    IEnumerator EnemySpawnTimer()
+    {
+        while (true)
+        {
+            // Spawn game object whereever the object of the script is located.
+            Instantiate(spawnEnemy, transform.position, Quaternion.identity);
+            // Wait for seconds befor continueing the loop.
+            yield return new WaitForSeconds(delayTime);
+        }
+    }
+}
