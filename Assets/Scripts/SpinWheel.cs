13eb812795fe81f8d2e927b9bdc74dc43819dbe5
TSantosFigueira <tsantosfigueira@outlook.com>
2015-10-28 11:47:23 -0400

    Avada Kedavra!

--- Assets/Scripts/SpinWheel.cs
+++ Assets/Scripts/SpinWheel.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class SpinWheel : MonoBehaviour
 {
@@ -7,49 +6,54 @@ public class SpinWheel : MonoBehaviour
     public float minSwipeDistX;
     private Vector2 startPos;
     public float power = 100; //the engine power applied to the wheel
-    public float velocity = 0.00000001f;
+    public float velocity = 0.1f;
+    RaycastHit hit;
     [SerializeField]
     GameObject car; //the object whose velocity you are calculating
 
     void Update()
     {
-
-        //#if UNITY_ANDROID
         if (Input.touchCount > 0)
         {
-
             Touch touch = Input.touches[0];
 
             switch (touch.phase)
             {
 
                 case TouchPhase.Began:
-
                     startPos = touch.position;
                     break;
 
-               case TouchPhase.Moved:
-
-                        float swipeDistHorizontal = (new Vector3(touch.position.x, 0, 0) - new Vector3(startPos.x, 0, 0)).magnitude;
-                       // var wheelpower = velocity * power * Time.deltaTime; //velocity of "car" * engine power
+                case TouchPhase.Moved:
+                    float swipeDistHorizontal = (new Vector3(touch.position.x, 0, 0) - new Vector3(startPos.x, 0, 0)).magnitude;
+                    // var wheelpower = velocity * power * Time.deltaTime; //velocity of "car" * engine power
 
                     if (swipeDistHorizontal > minSwipeDistX)
+                    {
+                        float swipeValue = Mathf.Sign(touch.position.x - startPos.x);
+
+                        if (swipeValue > 0)
                         {
-
-                            float swipeValue = Mathf.Sign(touch.position.x - startPos.x);
-
-                            if (swipeValue > 0)
-                            {
-                                transform.Rotate(0, 0, 2);
-                            }
-                            else if (swipeValue < 0)
-                            {
-                               transform.Rotate(0, 0, -2);
-                            }
+                            transform.Rotate(0, 0, 2);
                         }
-                  break;
-               }
-                    
-           }
-       } 
+                        else if (swipeValue < 0)
+                        {
+                            transform.Rotate(0, 0, -2);
+                        }
+                    }
+                    break;
+            }
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                print(hit.point.ToString());
+                //transform.Translate(hit.point - transform.position);
+            }
+
+        }
+    }
 }
