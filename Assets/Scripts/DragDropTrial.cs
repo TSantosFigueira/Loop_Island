1648fbad188bc9fe98407781913aacf2ffc9adce
TSantosFigueira <tsantosfigueira@outlook.com>
2015-10-29 16:24:40 -0400

    Agora o Drag & Drop tem movimenta??o diferente do Wheel.

--- Assets/Scripts/DragDropTrial.cs
+++ Assets/Scripts/DragDropTrial.cs
@@ -6,12 +6,13 @@ public class DragDropTrial : MonoBehaviour {
     private Vector3 offset;
     private Vector3 curScreenPoint;
     private Vector3 curPosition;
+    private static bool dragging = true;
 
     void OnMouseDown()
     {
         screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
         offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
-        Cursor.visible = false;
+        dragging = false;
     }
 
     void OnMouseDrag()
@@ -19,11 +20,17 @@ public class DragDropTrial : MonoBehaviour {
         curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
         curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
         transform.position = curPosition;
+        dragging = false;
     }
 
     void OnMouseUp()
     {
-        Cursor.visible = true;
+        dragging = true;
+    }
+
+    public static bool isDragged()
+    {
+        return dragging;
     }
 
 }
--- Assets/Scripts/SpinWheel.cs
+++ Assets/Scripts/SpinWheel.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class SpinWheel : MonoBehaviour
 {
@@ -8,8 +9,7 @@ public class SpinWheel : MonoBehaviour
     public float power = 100; //the engine power applied to the wheel
     public float velocity = 0.1f;
     RaycastHit hit;
-    [SerializeField]
-    GameObject car; //the object whose velocity you are calculating
+    public GameObject wheel; //the object whose velocity you are calculating
 
     void Update()
     {
@@ -25,13 +25,15 @@ public class SpinWheel : MonoBehaviour
                     break;
 
                 case TouchPhase.Moved:
+                    //Input.GetTouch(0).position
                     Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
                     float swipeDistHorizontal = (new Vector3(touch.position.x, 0, 0) - new Vector3(startPos.x, 0, 0)).magnitude;
                     // var wheelpower = velocity * power * Time.deltaTime; //velocity of "car" * engine power
 
-                    if (swipeDistHorizontal > minSwipeDistX && Physics.Raycast(ray, out hit))
-                    {
+                    if (swipeDistHorizontal > minSwipeDistX && Physics.Raycast(ray, out hit) && DragDropTrial.isDragged())
+                    { 
                         float swipeValue = Mathf.Sign(touch.position.x - startPos.x);
+                        //GetComponent<SphereCollider>().gameObject.transform.position;
 
                         if (swipeValue > 0)
                         {
