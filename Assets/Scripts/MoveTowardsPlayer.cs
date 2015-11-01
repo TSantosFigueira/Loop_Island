8c9c8e4045e17279833b429d5843e0e439cd1890
TSantosFigueira <tsantosfigueira@outlook.com>
2015-11-01 10:55:22 -0400

    Os inimigos agora vao em dire??o ?s extremidades; :) Avada Kedavra!

--- Assets/Scripts/MoveTowardsPlayer.cs
+++ Assets/Scripts/MoveTowardsPlayer.cs
@@ -2,11 +2,36 @@
 using System.Collections;
 
 public class MoveTowardsPlayer : MonoBehaviour {
-    [SerializeField] GameObject turret;
-    float speed = 5f;
+    private GameObject turret;
+    public float speed = 1f;
+    private int side;
+    private int total = 0;
+ 
+
+    // This function returns which side the turret game object has to go, considering it randomically
+    GameObject WhichSide(int side)
+    {
+        foreach (GameObject obj in GameObject.FindGameObjectsWithTag("dock"))
+        {
+            total += 1;
+            if (total == side) 
+            {
+                return obj;
+            }
+        }
+        return null;
+    }
+
+    void Start()
+    {
+        turret = WhichSide(Random.Range(1, 9));
+    }
 
 	// Update is called once per frame
 	void Update () {
-        transform.position = Vector2.MoveTowards(transform.position, turret.transform.position, speed * Time.deltaTime);
+        if (turret)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, turret.transform.position, speed * Time.deltaTime);
+        }
 	}
 }
