68d41f29670ebc67ce988a7e60aa5c4f4448b38d
DESKTOP-BP3LNCG\BrunaTatiane <zilda.taty5@gmail.com>
2015-11-02 14:14:01 -0200

    avada kedrava

---
+++ Assets/Scripts/GeneratePowerUp.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneratePowerUp : MonoBehaviour
+{
+    public GameObject gameObj;
+    public float primeiroSpawnEm;
+    public float tempoEntreSpawn;
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("CreateObstacle", primeiroSpawnEm, tempoEntreSpawn);
+    }
+    void CreateObstacle()
+    {
+        Instantiate(gameObj);
+
+    }
+}
