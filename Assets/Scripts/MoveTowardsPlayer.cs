cdd369e3889f12e707f6319f5305b94775423b62
TSantosFigueira <tsantosfigueira@outlook.com>
2015-11-03 14:58:39 -0400

    Agora os inimigos se direcionam ?s bordas do disco sem segui-lo quando o disco ? movimentado.

--- Assets/Scripts/MoveTowardsPlayer.cs
+++ Assets/Scripts/MoveTowardsPlayer.cs
@@ -5,20 +5,17 @@ public class MoveTowardsPlayer : MonoBehaviour {
     private GameObject turret;
     public float speed = 1f;
     private int side;
-    private int total = 0;
-    private GameObject target;
- 
+    private int total = 0; 
 
     // This function returns which side the turret game object has to go, considering it randomically
-    GameObject WhichSide(int side)
+   GameObject WhichSide(int side)
     {
         foreach (GameObject obj in GameObject.FindGameObjectsWithTag("dock"))
         {
             total += 1;
             if (total == side) 
             {
-                target = obj;
-                return target;
+                return obj;
             }
         }
         return null;
@@ -28,13 +25,9 @@ public class MoveTowardsPlayer : MonoBehaviour {
     {
         turret = WhichSide(Random.Range(1, 9));
     }
-
-	// Update is called once per frame
-	void Update () {
-        if (turret)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, new Vector2(turret.transform.position.x, turret.transform.position.y), speed * Time.deltaTime);
-        }
-        turret = null;
-	} 
+    
+    // Update is called once per frame
+    void Update () {
+        transform.position = Vector2.Lerp(transform.position, turret.transform.position, speed * Time.deltaTime);
+	}  
 }
